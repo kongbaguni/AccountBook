c98c1fe56c90510d27ce423ec8ff44c538a95dea
kongbaguni <kongbaguni@gmail.com>
2020-07-06 17:05:34 +0900

    Initial Commit

---
+++ AccountBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccountBook
+//
+//  Created by Changyul Seo on 2020/07/06.
+//  Copyright © 2020 Changyul Seo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
